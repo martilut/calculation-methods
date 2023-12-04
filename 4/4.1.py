d025b0b1903ac82c051d588dd4a9c670cae34e01
martilut <jeykolton@gmail.com>
2023-12-04 19:54:08 +0300

    added 4.1 and 4.2

--- 4/4.1.py
+++ 4/4.1.py
@@ -1,9 +1,8 @@
 import math
-import numpy
 import scipy
 
 
-def simpson(a, b):
+def simpson(func, a, b):
     param = (b - a) / 6.0
     sum1 = func(a)
     sum2 = 4 * func((a + b) / 2.0)
@@ -12,7 +11,7 @@ def simpson(a, b):
     return param * (sum1 + sum2 + sum3)
 
 
-def formula38(a, b):
+def formula38(func, a, b):
     param = (b - a)
     h = (b - a) / 3.0
 
@@ -24,40 +23,78 @@ def formula38(a, b):
     return param * (sum1 + sum2 + sum3 + sum4)
 
 
-def res(func, a, b):
-    exact = scipy.integrate.quad(func, a, b)
-    exact_value = exact[0]
-    print(f"\nТочное значение: {exact_value}")
+def left_rect(func, a, b, exact_value):
     leftRect = (b - a) * func(a)
     leftRectError = abs(exact_value - leftRect)
-    print(f"КФ левого прямоугольника: {leftRect}, ошибка: {leftRectError}")
+    return leftRect, leftRectError
+
+
+def right_rect(func, a, b, exact_value):
     right_rect = (b - a) * func(b)
     right_rect_err = abs(exact_value - right_rect)
-    print(f"КФ правого прямоугольника: {right_rect}, ошибка: {right_rect_err}")
+    return right_rect, right_rect_err
+
+
+def center(func, a, b, exact_value):
     center = (b - a) / 2.0
     center_rect = (b - a) * func(center)
     center_rect_err = abs(exact_value - center_rect)
-    print(f"КФ среднего прямоугольника: {center_rect}, ошибка: {center_rect_err}")
+    return center_rect, center_rect_err
+
+
+def trapezoid(func, a, b, exact_value):
     valSum = func(a) + func(b)
     trapezoid = (b - a) / 2.0 * valSum
     trapezoid_err = abs(exact_value - trapezoid)
-    print(f"КФ трапеции: {trapezoid}, ошибка: {trapezoid_err}")
-    simpson = simpson(a, b)
-    simpson_err = abs(exact_value - simpson)
-    print(f"КФ Симпсона: {simpson}, ошибка: {simpson_err}")
-    formula38 = formula38(a, b)
-    formula38_err = abs(exact_value - formula38)
-    print(f"КФ 3/8: {formula38}, ошибка: {formula38_err}")
+    return trapezoid, trapezoid_err
+
+
+def calculate(func, a, b):
+    exact = scipy.integrate.quad(func, a, b)
+    exact_value = exact[0]
+    print(f"\nТочное значение: {exact_value}")
+    leftRect, leftRectError = left_rect(func, a, b, exact_value)
+    print(f"КФ левого прямоугольника: {leftRect}, ошибка: {leftRectError}")
+    right_rect_val, right_rect_err = right_rect(func, a, b, exact_value)
+    print(f"КФ правого прямоугольника: {right_rect_val}, ошибка: {right_rect_err}")
+    center_rect, center_rect_err = center(func, a, b, exact_value)
+    print(f"КФ среднего прямоугольника: {center_rect}, ошибка: {center_rect_err}")
+    trapezoid_val, trapezoid_err = trapezoid(func, a, b, exact_value)
+    print(f"КФ трапеции: {trapezoid_val}, ошибка: {trapezoid_err}")
+    simpson_val = simpson(func, a, b)
+    simpson_err = abs(exact_value - simpson_val)
+    print(f"КФ Симпсона: {simpson_val}, ошибка: {simpson_err}")
+    formula38_val = formula38(func, a, b)
+    formula38_err = abs(exact_value - formula38_val)
+    print(f"КФ 3/8: {formula38_val}, ошибка: {formula38_err}")
 
 
 if __name__ == '__main__':
-    print('Задача приближенного вычисления определенного интеграла')
-    print('f(x) = e^(x) * sqrt(1 - x)')
-
-    func = lambda x: math.exp(x) * math.sqrt(1 - x)
-    fx = lambda x: math.exp(x)
-    px = lambda x: math.sqrt(1 - x)
+    print('Задача приближенного вычисления определенного интеграла по квадратурным формулам')
+    print('f(x) = e^(x)')
+    e_x = lambda x: math.e ** x
 
     print('Введите [A, B]')
     a = float(input('Введите A: '))
     b = float(input('Введите B: '))
+    calculate(e_x, a, b)
+
+    polynom_0 = lambda x: 100
+    polynom_1 = lambda x: x ** 1
+    polynom_2 = lambda x: x ** 2
+    polynom_3 = lambda x: x ** 3
+
+    print("------------------------------------------------")
+    print("Проверка на полиномах")
+    print("------------------------------------------------")
+    print('f(x) = 100')
+    calculate(polynom_0, a, b)
+    print("------------------------------------------------")
+    print('f(x) = x')
+    calculate(polynom_1, a, b)
+    print("------------------------------------------------")
+    print('f(x) = x^2')
+    calculate(polynom_2, a, b)
+    print("------------------------------------------------")
+    print('f(x) = x^3')
+    calculate(polynom_3, a, b)
---
+++ 4/4.2.py
@@ -0,0 +1,175 @@
+import math
+
+import scipy
+
+
+class MethodData:
+    def __init__(self, name, exact_value, value, theoretical_error):
+        self.name = name
+        self.exact_value = exact_value
+        self.value = value
+        self.abs_error = abs(exact_value - value)
+        self.rel_error = abs(self.abs_error / exact_value) if exact_value != 0 else 0
+        self.theoretical_error = theoretical_error
+
+
+class CalculationData:
+    def calculate_data(self, func, func_int, a, b, step, parts):
+        exact_integral = scipy.integrate.quad(func_int, a, b)
+        exact_value = exact_integral[0]
+        left_rect = leftRectangle(func, a, step, parts)
+        left_rect_theor_err = ((b - a) ** 2) / (2 * parts) * abs(math.exp(b))
+        left_rect_data = MethodData("Left rectangle", exact_value, left_rect, left_rect_theor_err)
+        right_rect = rightRectangle(func, a, step, parts)
+        right_rect_theor_err = ((b - a) ** 2) / (2 * parts) * abs(math.exp(b))
+        right_rect_data = MethodData("Right rectangle", exact_value, right_rect, right_rect_theor_err)
+        center_rect = centerRectangle(func, a, step, parts)
+        center_rect_theor_err = ((b - a) ** 3) / (24 * parts ** 2) * abs(math.exp(b))
+        center_rect_data = MethodData("Center rectangle", exact_value, center_rect, center_rect_theor_err)
+        trapez = trapezoid(func, a, b, step, parts)
+        trapez_theor_err = ((b - a) ** 3) / (12 * parts ** 2) * abs(math.exp(b))
+        trapez_data = MethodData("Trapezoid", exact_value, trapez, trapez_theor_err)
+        simp = simpson(func, a, b, step, parts)
+        simp_theor_err = ((b - a) ** 5) / (2880 * parts ** 4) * abs(math.exp(b))
+        simp_data = MethodData("Simpson", exact_value, simp, simp_theor_err)
+        return [left_rect_data, right_rect_data, center_rect_data, trapez_data, simp_data]
+
+
+def print_calculation_data(stats, theor_err=False):
+    for data in stats:
+        print(f"---{data.name}---")
+        print("Exact value: " + str(data.exact_value))
+        print("Value: " + str(data.value))
+        print("Absolute error: " + str(data.abs_error))
+        print("Relative error: " + str(data.rel_error))
+        if theor_err:
+            print("Theoretical error: " + str(data.theoretical_error))
+        print()
+
+
+def leftRectangle(func, a, step, parts):
+    res = 0
+    for j in range(0, parts):
+        arg = a + j * step
+        res += func(arg)
+    return step * res
+
+
+def rightRectangle(func, a, step, parts):
+    res = 0
+    for j in range(0, parts):
+        arg = a + (j + 1) * step
+        res += func(arg)
+    return step * res
+
+
+def centerRectangle(func, a, step, parts):
+    res = 0
+    for j in range(0, parts):
+        arg = a + (j + 0.5) * step
+        res += func(arg)
+    return step * res
+
+
+def trapezoid(func, a, b, step, parts):
+    res = func(a) + func(b)
+    for j in range(1, parts):
+        arg = a + j * step
+        res += 2 * func(arg)
+    return 0.5 * step * res
+
+
+def simpson(func, a, b, step, parts):
+    res = func(a) + func(b)
+    for j in range(1, parts):
+        arg = a + j * step
+        res += 2 * func(arg)
+    for j in range(0, parts):
+        arg = a + (j + 0.5) * step
+        res += 4 * func(arg)
+    return (step / 6.0) * res
+
+
+def refineStats(start_data, new_data, L):
+    stats = []
+    for i in range(0, len(start_data)):
+        start_current = start_data[i]
+        new_current = new_data[i]
+        if start_current.name == 'Среднего пр-ка' or start_current.name == 'Трапеции':
+            d = 1
+        elif start_current.name == 'Симпсона':
+            d = 3
+        else:
+            d = 0
+        value_refined = ((L ** (d + 1)) * new_current.value - start_current.value) / ((L ** (d + 1)) - 1)
+        value_exact = start_current.exactValue
+        refinedStats = MethodData(start_current.name, value_exact, value_refined, None)
+        stats.append(refinedStats)
+    return stats
+
+
+if __name__ == '__main__':
+    print('Задача приближенного вычисления интеграла по составным квадратурным формулам')
+    print('f(x) = exp(x)')
+    print()
+
+    f = lambda x: math.exp(x)
+    p = lambda x: 1
+    func_int = lambda x: f(x) * p(x)
+
+    polynom_0 = lambda x: 100
+    polynom_1 = lambda x: x ** 1
+    polynom_2 = lambda x: x ** 2
+    polynom_3 = lambda x: x ** 3
+
+    a = float(input('Введите A: '))
+    b = float(input('Введите B: '))
+
+    print()
+    parts = int(input('Введите число разбиений [A, B]: '))
+
+    start_steps = (b - a) / float(parts)
+    start_stats = CalculationData().calculate_data(f, func_int, a, b, start_steps, parts)
+
+    print()
+    print('m = ', parts)
+    print('h =', start_steps)
+    print_calculation_data(start_stats, True)
+    print("------------------------------------------------")
+    print("Проверка на полиномах")
+    print("------------------------------------------------")
+
+    print('f(x) = 100')
+    polynom_0_data = CalculationData().calculate_data(polynom_0, polynom_0, a, b, start_steps, parts)
+    print_calculation_data(polynom_0_data)
+    print("------------------------------------------------")
+
+    print('f(x) = x')
+    polynom_1_data = CalculationData().calculate_data(polynom_1, polynom_1, a, b, start_steps, parts)
+    print_calculation_data(polynom_1_data)
+    print("------------------------------------------------")
+
+    print('f(x) = x^2')
+    polynom_2_data = CalculationData().calculate_data(polynom_2, polynom_2, a, b, start_steps, parts)
+    print_calculation_data(polynom_2_data)
+    print("------------------------------------------------")
+
+    print('f(x) = x^3')
+    polynom_3_data = CalculationData().calculate_data(polynom_3, polynom_3, a, b, start_steps, parts)
+    print_calculation_data(polynom_3_data)
+
+    print("------------------------------------------------")
+    L = int(input('Введите L: '))
+    print("------------------------------------------------")
+
+    parts *= L
+    steps_new = (b - a) / float(parts)
+    data_new = CalculationData().calculate_data(f, func_int, a, b, steps_new, parts)
+    print()
+    print('h1 =', start_steps)
+    print_calculation_data(start_stats)
+    print("------------------------------------------------")
+
+    print()
+    print('h2 =', steps_new)
+    print_calculation_data(data_new)
